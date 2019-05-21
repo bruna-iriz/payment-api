1e9ef353eb151821d9b236744626291d716f7904
bruna-iriz <bruna.iriz@hotmail.com>
2019-05-21 04:20:36 -0300

    Adicionando requisição GET buscando por ID em Buyer

--- src/main/java/br/com/payment/api/resource/BuyerResource.java
+++ src/main/java/br/com/payment/api/resource/BuyerResource.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,33 +25,34 @@ import br.com.payment.api.services.BuyerServices;
 public class BuyerResource {
 
 	@Autowired
-	private BuyerRepository buyerRepository;	
-	
+	private BuyerRepository buyerRepository;
+
 	@Autowired
-	private BuyerServices  buyerServices;
-	
+	private BuyerServices buyerServices;
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
+	// Listando todos os Buyers
 	@GetMapping
 	public List<Buyer> listBuyers() {
 		return buyerRepository.findAll();
 
 	}
-	
-	/*@PostMapping("/buyers")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void createBuyer(@RequestBody Buyer buyer) {
-		return buyerRepository.save(buyer);
-	}*/
-	
-	//Cadas
+
+	// Listando Buyers por ID
+	@GetMapping("/{idBuyer}")
+	public Buyer searchBuyerId(@PathVariable Long idBuyer) {
+		return buyerRepository.findOne(idBuyer);
+	}
+
+	// Criando Buyer
 	@PostMapping
-	public ResponseEntity<Buyer>create(@RequestBody Buyer buyer, HttpServletResponse response){
+	public ResponseEntity<Buyer> create(@RequestBody Buyer buyer, HttpServletResponse response) {
 		Buyer buyerSave = buyerServices.save(buyer);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, buyerSave.getIdBuyer()));
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(buyerSave);
-		
 	}
 
 }
