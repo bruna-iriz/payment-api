75457fa2b26aadc2b03fd3ed9adcf3093b0e8e63
bruna-iriz <bruna.iriz@hotmail.com>
2019-05-25 15:01:18 -0300

    Implementando a requisição DELETE em Buyer e Card

--- src/main/java/br/com/payment/api/resource/BuyerResource.java
+++ src/main/java/br/com/payment/api/resource/BuyerResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.payment.api.event.ResourceCreateEvent;
@@ -43,7 +45,7 @@ public class BuyerResource {
 
 	// Listando Buyers por ID
 	@GetMapping("/{id}")
-	public Buyer searchBuyerId(@PathVariable Long id) {
+	public Buyer searchBuyerId(@PathVariable(value = "id") Long id) {
 		return buyerRepository.findOne(id);
 	}
 
@@ -55,5 +57,19 @@ public class BuyerResource {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(buyerSave);
 	}
+
+	// Deletando Buyer
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		buyerRepository.delete(id);
+	}
+
+	/*
+	 * @ApiOperation(value="Atualiza um buyer")
+	 * 
+	 * @PutMapping("/buyers") public Buyer atualizaBuyer(@RequestBody @Valid
+	 * Buyer buyer) { return buyerRepository.save(buyer); }
+	 */
 
 }
--- src/main/java/br/com/payment/api/resource/CardResource.java
+++ src/main/java/br/com/payment/api/resource/CardResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.payment.api.event.ResourceCreateEvent;
@@ -57,4 +59,17 @@ public class CardResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(cardSave);
 	}
 
+	// Deletando Card
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		cardRepository.delete(id);
+	}
+
+	// Atualizar conta passando código através do Put
+	/*
+	 * @PutMapping("/{id}") public ResponseEntity<Card> atualizar(@PathVariable
+	 * Long id, @Valid @RequestBody Card card) { Card cardUpdate =
+	 * cardServices.update(id, card); return ResponseEntity.ok(cardUpdate); }
+	 */
 }
